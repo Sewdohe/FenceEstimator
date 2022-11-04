5a78e27bf234a12d436418b012a0b6ec92198f59
sewdohe <sewdohe@protonmail.com>
2022-11-04 19:55:36 -0400

    First commit added angularFIre & angularMaterial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fence-estimator-c533e',
+    appId: '1:59825157599:web:0aaa22da155d9ab651406a',
+    storageBucket: 'fence-estimator-c533e.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCkAOgQ2Ge2wLsqaW44wAS_8YCeHWV6afM',
+    authDomain: 'fence-estimator-c533e.firebaseapp.com',
+    messagingSenderId: '59825157599',
+  },
   production: true
 };
