a2111a96c5b34a53189bd5d7cfcd2a5f3c9f3b82
sewdohe <sewdohe@protonmail.com>
2022-11-04 20:24:16 -0400

    loading material data

---
+++ src/app/interfaces.ts
@@ -0,0 +1,7 @@
+// ALL COST ARE IN sq FT or SOMETHING
+// we need a standard for costs
+export interface Material {
+  name: string;
+  cost: number;
+  icon?: string; // TODO: figure out material display
+}
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from '@firebase/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Material } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  //TODO: Implement material selection
+  // by calling selectedMaterial = new Behaviorsubject(initalMaterial)
+  selectedMaterial: BehaviorSubject<Material | null> | null = null;
+  materials$: Observable<Material[]> | null = null;
 
-  constructor() { }
+  constructor(private firestore: Firestore) {
+    let materialsCollection = collection(this.firestore, 'materials')
+    //@ts-ignore
+    this.materials$ = collectionData(materialsCollection);
+    this.materials$?.subscribe((materials: Material[]) => {
+      console.log(materials)
+      // @ts-ignore
+        this.selectedMaterial = new BehaviorSubject(materials[0]);
+    })
+    
+    this.selectedMaterial?.subscribe(material => {
+      console.log(`selected ${material?.name}`)
+    })
+  }
 }
